6cc76bba47804f487c50ed4fe1b56f7c9d514d95
ksleima <ksleima@us.ibm.com>
2017-05-31 08:51:54 -0400

    & handling in test env

--- service/westfield_data_service.js
+++ service/westfield_data_service.js
@@ -588,13 +588,19 @@ exports.cognitiveOrchestrator = function(res,details,callback){
 							reject(policyDetails);
 						}else{
 							if(policyDetails.namedInsured != undefined){
-								props.namedInsured 		= policyDetails.namedInsured.replace("&", "AND");
+								props.namedInsured 		= policyDetails.namedInsured.replace(" &amp; ", " AND ");
+								props.namedInsured 		= props.namedInsured.replace("&amp;", " AND ");
+								props.namedInsured 		= policyDetails.namedInsured.replace(" & ", " AND ");
+								props.namedInsured 		= props.namedInsured.replace("&", " AND ");
 							}
 							props.numberOfVehicles 	= policyDetails.numberOfVehicles;
 							props.numberOfDrivers 	= policyDetails.numberOfDrivers;
 							props.driversUnder25 	= policyDetails.driversUnder25;
 							if(policyDetails.agency != undefined){
-								props.agency 		= policyDetails.agency.replace("&", "AND");
+								props.agency 		= policyDetails.agency.replace(" &amp; ", " AND ");
+								props.agency 		= props.agency.replace("&amp;", " AND ");
+								props.agency 		= policyDetails.agency.replace(" & ", " AND ");
+								props.agency 		= props.agency.replace("&", " AND ");
 							}
 							props.hasTrailers 		= policyDetails.hasTrailers;
 							resolve(props);
